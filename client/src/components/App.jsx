c8b04661839b6a8b14d76392b27578fd9bbb9c5f
Lenora Esquenazi <lenaciousd@gmail.com>
2021-10-13 15:47:42 -0700

    imported necessary components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import CounselingTypesList from './CounselingTypesList.jsx'
+import AboutMe from './AboutMe.jsx'
+import Reviews from './Reviews.jsx'
 
 const App = () => {
-  return (<div> Welcome! </div>)
+
+  return (
+  <div>
+    <h1>Growth & Empowerment Life Coaching</h1>
+    <div>
+      <h1> Welcome! </h1>
+    </div>
+    <CounselingTypesList />
+    <AboutMe />
+    <Reviews />
+  </div>
+  )
 }
 
 export default App
