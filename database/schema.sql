67b0a295d9fa8d44a82b00df0790510a2ba9cdaa
Lenora Esquenazi <lenaciousd@gmail.com>
2021-10-13 15:50:17 -0700

    Created schemafor reviews table

---
+++ database/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE growthAndEmpowerment;
+
+CREATE TABLE reviews (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  description VARCHAR(600),
+  category VARCHAR(40),
+  age VARCHAR(20),
+  gender VARCHAR(30)
+);
+
