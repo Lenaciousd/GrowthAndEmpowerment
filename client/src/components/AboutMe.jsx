f831253b1bfc68bce615746eac0a60c03d78a9a1
Lenora Esquenazi <lenaciousd@gmail.com>
2021-10-13 15:49:47 -0700

    Added an About me component

---
+++ client/src/components/AboutMe.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+// import Img from '../../../assets/lenoraOnPath.jpg'
+
+const AboutMe = () => {
+
+  return (
+    <div className="aboutMe">
+      <h2>About Lenora</h2>
+      <img src="" alt="Lenora"/>
+      <p>
+        I have spent most of my life as a childcare professional working with children and their parents. I believe that if children are treated with respect and are taught important skills such as problem solving, critical thinking, open mindedness, empathy, and active listening, that our future generations will have much fewer problems. I received a Bachelor's Degree in Psychology from the University of California, San Diego. In addition to Psychology, I have also studied relationships and marriage, early childhood education, parenting, health, nutrition, well being, finding purpose, etc. I have experience helping people with relationship concerns, providing parenting advice and tips, planning out eating and fitness goals, reducing stress, looking at their lives in a new way, and general goal setting while providing motivation and support. I believe my purpose in life is to be a lifelong learner, a catalyst of positive change, and a person with meaningful relationships. To be someone that shares insights and reminds people of their own agency. To be someone that has fun and enjoys all that the world has to offer.
+      </p>
+      <p>
+        I often think about how to improve my life, and other people's lives. And I often think about what new systems or changes could be implemented to make all of our lives better and more fulfilling. To make the world a better place we have to start by making ourselves better people. I believe everyone at any age has the potential to learn and grow in all areas of life. Many of us get trapped in a mindset of feeling stuck with whatever our lives currently are. But we have more control than you may think. Studies show that 50% of our happiness comes from genetics, 10% comes from circumstances and things that happen to us, while the other 40% is our own thoughts, mindset, and actions. You may not have control of how you were born or what happens to you, but you do have control over what you focus on, what you put your time and effort into, which people you spend time with, whose opinion you listen to, how you interpret what happens and even how you want to react to it. Believing things can change for the better is the first step. Then you must decide what matters most to you and put your all into it.
+      </p>
+    </div>
+  )
+}
+
+export default AboutMe
