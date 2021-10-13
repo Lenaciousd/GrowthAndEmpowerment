8464bda43ed59955abb545e4e4e3bb62779182ae
Lenora Esquenazi <lenaciousd@gmail.com>
2021-10-13 15:42:56 -0700

    Added get, post, and delete request handling

--- server/index.js
+++ server/index.js
@@ -1,11 +1,46 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3002
+const db = require('../database/index.js')
+const bodyParser = require('body-parser')
 
 app.use(express.static('client/dist'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Goodbye World!')
+})
+
+app.get('/reviews', (req, res) => {
+  db.getAllReviews(req, (error, data) => {
+    if (error) {
+      console.log('get request failed ', error)
+    } else {
+      res.send(data)
+    }
+  })
+})
+
+app.post('/reviews', (req, res) => {
+  db.postReview(req, (error, data) => {
+    if (error) {
+      console.log('post request failed ', error)
+    } else {
+      console.log(data)
+      res.send(data)
+    }
+  })
+})
+
+app.delete('/reviews', (req, res) => {
+  db.deleteReview(req, (error, data) => {
+    if (error) {
+      console.log('delete request failed ', error)
+    } else {
+      res.send(data)
+    }
+  })
 })
 
 app.listen(port, () => {
