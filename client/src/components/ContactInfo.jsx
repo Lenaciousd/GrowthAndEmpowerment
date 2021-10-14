3a420add74d802baa1a577e961884461d92ae261
Lenora Esquenazi <lenaciousd@gmail.com>
2021-10-13 21:37:36 -0700

    Added a contact me component

---
+++ client/src/components/ContactInfo.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const ContactInfo = () => {
+   return (
+    <div className="container-contact" id="Contact">
+      <h2>
+        Contact Me
+      </h2>
+      <p>
+        {/* <div>
+          <a href="mailto:lenoracare@gmail.com" target="_blank" rel="noopener noreferrer"><img
+            src="Gmail Icon.png" width="80" height="60"></a>
+
+          <a href="https://www.linkedin.com/in/lenora-esquenazi-a6278134" target="_blank"
+            rel="noopener noreferrer"><img src="Linkedin Logo.webp" width="60" height="60"></a>
+          <a href="https://www.facebook.com/groups/1153363065126869" target="_blank"
+            rel="noopener noreferrer"><img src="Facebook Logo.png" width="80" height="60"></a>
+        </div> */}
+      </p>
+    </div>
+  )
+}
+
+export default ContactInfo;
